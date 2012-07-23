a1189020f7b6ff67e35ec47e2994eddd391d8363
naoranger <naoranger@gmail.com>
2012-07-23 22:32:42 +0900

    memcachier

--- app.rb
+++ app.rb
@@ -5,12 +5,12 @@ require './lib/mongoid'
 require './models/info'
 require './models/event'
 
+=begin
 require 'addressable/uri'
 require 'open-uri'
 require 'json'
 include Addressable
-
-# require 'icalendar'
+=end
 
 =begin
 require './lib/image_uploader'
@@ -25,16 +25,16 @@ use ServeGridfsImage
 enable :sessions
 set :raise_errors, false
 set :show_exceptions, false
-set :cache, Dalli::Client.new
+set :cache, Dalli::Client.new(ENV["MEMCACHIER_SERVERS"], {username: ENV["MEMCACHIER_USERNAME"], password: ENV["MEMCACHIER_PASSWORD"]})
 set :haml, :format => :html5
 
 if ENV['MEMCACHIER_SERVERS']
   use Rack::Cache,
-    verbose:     true,
-    default_ttl: 60 * 60,
-    metastore:   Dalli::Client.new,
-    entitystore: ENV['MEMCACHIER_SERVERS'] ? "memcached://#{ENV['MEMCACHIER_SERVERS']}/body" : 'file:tmp/cache/rack/body',
-    allow_reload: false
+	  verbose:     true,
+	  default_ttl: 60 * 60 * 24,
+	  metastore:   ENV['MEMCACHIER_SERVERS'] ? "memcached://#{ENV['MEMCACHIER_SERVERS']}/meta" : 'file:tmp/cache/rack/meta',
+	  entitystore: ENV['MEMCACHIER_SERVERS'] ? "memcached://#{ENV['MEMCACHIER_SERVERS']}/body" : 'file:tmp/cache/rack/body',
+	  allow_reload: false
 end
 
 not_found do
@@ -42,24 +42,11 @@ not_found do
 end
 
 configure do
-  set :static_cache_control => [:public, :max_age => 60*60*24*30]
-  # load File.expand_path('../lib/mongoid.rb', __FILE__)
-  # load File.expand_path('../lib/carrierwave.rb', __FILE__)
+  set :static_cache_control => [:public, :max_age => 60*60*24]
 end
 
 before do
-=begin
-  if settings.environment == :production && request.scheme != 'https'
-    redirect "https://#{request.env['HTTP_HOST']}"
-  end
-=end
-=begin
-  unless session[:date]
-    session[:date] = Date.today
-  end
-  @date = session[:date]
-  @wdays = ['日','月','火','水','木','金','土']
-=end
+
 end
 
 helpers do
@@ -80,64 +67,40 @@ helpers do
   end
   
   def events
-    events = Event.where(:category => 'atnd')
-    @events = events.sort_by{ |item| item['dtstart'].to_i }
+    @events ||= settings.cache.fetch(Date.today.to_s) do
+      events = Event.where(category: 'atnd')
+      events = events.sort_by{ |item| item['dtstart'].to_i}
+      settings.cache.set(Date.today.to_s, events, 60*60*24)
+      events
+    end
   end
   
-  def search_all
-    @info = []
-    Info.all_of(object: 'link').each_with_index do |info, i|
-      @info[i/3] = [] if i % 3 == 0
-      @info[i/3][i%3] = info
-    end
-    
-=begin
-    Event.all_of(category: 'google').each do |event|
-      p rrule = event.rrule.scan(/(\w+)\=([\w,]+)/)
-      case rrule.assoc("FREQ")[1]
-      when "WEEKLY"
-        rrule.assoc("UNTIL")
-      end
-    end
-=end
-=begin
-      p event.name
-      p event.description
-      p event.dtstart
-      p event.dtend
-      p event.rrule.scan(/(\w+)\=([\w,]+)/)
-=end
+  def events_no_cache
+    @events = Event.where(category: 'atnd')
   end
   
-  def get_google_calendar
-    url = "https://www.google.com/calendar/ical/mroq255j953tfk6jsu2gf77trk%40group.calendar.google.com/public/basic.ics"
-    uri = URI.parse(url)
-    ical = open(uri).read
-    cal = Icalendar.parse(ical, true)
-    cal.events.each do |event|
-      p event.summary
-      p event.dtstart
-      p event.dtend
-      p event.description
-      r = ""
-      event.recurrence_rules.map do |rule|
-        p rule.orig_value.scan(/(\w+)\=([\w,]+)/)
-        r << rule.orig_value.to_s
+  def infomation
+    @info ||= settings.cache.fetch(Date.today.to_s) do
+      info = []
+      Info.all_of(object: 'link').each_with_index do |item, i|
+        info[i/3] = [] if i % 3 == 0
+        info[i/3][i%3] = item
       end
+      info
     end
   end
 end
 
 get "/" do
+  cache_control :public, max_age: 60 * 60 * 24
+  
   events
-  search_all
-#  get_google_calendar
+  infomation
   
   sass :style
   haml :index 
 end
 
-# used by Canvas apps - redirect the POST to be a regular GET
 post "/" do
   redirect "/"
 end
